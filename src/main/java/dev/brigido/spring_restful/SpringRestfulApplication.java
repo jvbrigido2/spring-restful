373220ca807f7bfbbc76287876476a9075ee6701
brigido <jv_brigido@hotmail.com>
2024-09-06 22:23:51 -0400

    Setup the project

---
+++ src/main/java/dev/brigido/spring_restful/SpringRestfulApplication.java
@@ -0,0 +1,13 @@
+package dev.brigido.spring_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulApplication.class, args);
+	}
+
+}
